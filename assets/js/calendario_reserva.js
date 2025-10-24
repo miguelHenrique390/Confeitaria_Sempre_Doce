047fbaa13ed853eeddb847649080b13fd5b5d01b
miguelHenrique390 <miguel.santos27012009@gmail.com>
2025-10-24 10:15:28 -0300

    alteracoes calendarios

--- assets/js/calendario_reserva.js
+++ assets/js/calendario_reserva.js
@@ -12,35 +12,35 @@ const opcoes = {
   eventClick: function(info) {
     const evento = info.event;
 
-    // Acessa os dados personalizados
+    
     const nome = evento.title;
     const telefone = evento.extendedProps.telefone;
     const produto = evento.extendedProps.produto;
     const quantidade = evento.extendedProps.quantidade;
-    const adicionais = evento.extendedProps.adicionais;
-    const data = evento.extendedProps.start
+    const comentario = evento.extendedProps.comentario;
+    const data = evento.start;
 
-    // Exibe os dados em um alerta (pode trocar por modal se quiser)
+    
     alert(
       ` Reserva: ${nome}\n` +
       ` Telefone: ${telefone}\n` +
       ` Produto: ${produto}\n` +
       ` Quantidade: ${quantidade}\n` +
-      ` Informações adicionais: ${adicionais}`+
-      `Data da reserva: ${data}`
+      ` Informações adicionais: ${comentario}\n`+
+      `Data da reserva: ${data}\n`
     );
 
-    info.jsEvent.preventDefault(); // Impede redirecionamento, se houver
+    info.jsEvent.preventDefault(); 
   }
 };
 
 const reservaJS = new FullCalendar.Calendar(calendario_r, opcoes);
 
-// Buscando os compromissos no LS
+
 let reservaLS = window.localStorage.getItem('reservas');
 reservaLS = JSON.parse(reservaLS);
 
-// Adiciona os eventos no calendário
+
 for (let i = 0; i < reservaLS.length; i++) {
   const reserva = reservaLS[i];
 
@@ -51,7 +51,7 @@ for (let i = 0; i < reservaLS.length; i++) {
       telefone: reserva['telefone'],
       produto: reserva['produto'],
       quantidade: reserva['quantidade'],
-      adicionais: reserva['adicionais']
+      comentario: reserva['comentario']
       
     }
   });
